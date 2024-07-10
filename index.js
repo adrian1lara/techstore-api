5c51a09416e8cf121d7c2e503b42091ce6934f2c
Adrian Lara <adrianlara203@gmail.com>
2024-07-09 23:02:56 -0500

    add orden endpoint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const rutaCliente = require('./routes/rutaCliente');
 const dbconnection = require('./db/connection');
 const rutaCategoria = require('./routes/rutaCategoria');
 const rutaProducto = require('./routes/rutaProducto');
+const rutaOrden = require('./routes/rutaOrdenes');
 const app = express();
 
 const port = 3000;
@@ -20,6 +21,8 @@ app.use("/api/cliente", rutaCliente);
 app.use("/api/categoria", rutaCategoria);
 
 app.use("/api/producto", rutaProducto);
+
+app.use("/api/ordenes", rutaOrden);
 
 app.listen(port, () => {
     console.log(`El servidor esta escuchando en el puerto http://localhost:${port}`)
